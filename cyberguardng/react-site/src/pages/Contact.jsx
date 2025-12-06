210e28999490d576de24b6f64b1f6c64cb2d432f
Osayande <oyande.a@gmail.com>
2025-12-06 15:41:55 -0500

    Fix contact form endpoint

--- cyberguardng/react-site/src/pages/Contact.jsx
+++ cyberguardng/react-site/src/pages/Contact.jsx
@@ -19,7 +19,7 @@ export default function Contact() {
     };
 
     try {
-      const response = await fetch("/functions/contact", {
+      const response = await fetch("/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
